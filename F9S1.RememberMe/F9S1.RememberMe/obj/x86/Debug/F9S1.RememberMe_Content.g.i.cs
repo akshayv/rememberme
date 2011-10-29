ca4d97874a959c96a5141f89f1add1393056c3f5
Inian Parameshwaran <inian1234@gmail.com>
2011-10-30 05:42:19 +0800

    starred in datagrid icon added fr our productt..run it and see..

--- F9S1.RememberMe/F9S1.RememberMe/obj/x86/Debug/F9S1.RememberMe_Content.g.i.cs
+++ F9S1.RememberMe/F9S1.RememberMe/obj/x86/Debug/F9S1.RememberMe_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("delete.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("logo1.png")]
 
 
